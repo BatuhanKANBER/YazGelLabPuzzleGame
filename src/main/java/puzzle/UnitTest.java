1fa12b9a7e832e7cd666647e1d8c8ff5bb80be0d
Batuhan KANBER <104573473+BatuhanKANBER@users.noreply.github.com>
2023-11-22 15:19:31 +0300

    Update UnitTest.java

--- src/main/java/puzzle/UnitTest.java
+++ src/main/java/puzzle/UnitTest.java
@@ -8,7 +8,7 @@ public class UnitTest {
 
     // Test for the bestScoreWriter function
     @Test
-    public void testBestScoreWriter() java.io.FileNotFoundException{
+    public void testBestScoreWriter() throws java.io.FileNotFoundException{
         // Create an instance of your class
         GameScreen gameScreen = new GameScreen();
 
