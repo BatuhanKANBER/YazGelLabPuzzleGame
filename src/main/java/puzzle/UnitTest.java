a5e0806e1d89466fba4eba92fd1815832506f66a
Batuhan KANBER <batuhankanber28@gmail.com>
2023-11-22 14:35:34 +0300

    feat : UnitTest class added.

---
+++ src/main/java/puzzle/UnitTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    // Test for the bestScoreWriter function
+    @Test
+    public void testBestScoreWriter() {
+        // Create an instance of your class
+        GameScreen gameScreen = new GameScreen();
+
+        // Define a line for testing
+        String testLine = "Test Line";
+
+        // Call the bestScoreWriter function and check for exceptions
+        assertDoesNotThrow(() -> {
+            gameScreen.bestScoreWriter(testLine);
+        });
+    }
+}
